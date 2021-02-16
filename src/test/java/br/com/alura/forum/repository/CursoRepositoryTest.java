3a7429acc5f116daa99feca094ccee27ff317504
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-02-15 23:41:00 -0200

    EntityManagerTest

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,10 +24,19 @@ public class CursoRepositoryTest {
 	
 	@Autowired
 	private CursoRepository repository;
+	
+	@Autowired
+	private TestEntityManager em;
 
 	@Test
 	public void CarregarCurso() {
 		String nomeCurso ="HTML 5";
+		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("programacao");
+		em.persist(html5);
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
 		Assert.assertEquals(nomeCurso, curso.getNome());
